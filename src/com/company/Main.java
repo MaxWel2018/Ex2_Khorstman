5fe73a74f18cb4609174f76bf1484459496fd6ab
maxwel2018 <Krugovy@mail.ru>
2018-10-23 11:53:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int n = scanner.nextInt();
+        int b = random.nextInt();
+    }
+}
